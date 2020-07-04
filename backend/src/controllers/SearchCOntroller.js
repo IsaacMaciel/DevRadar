406cd8ff3677fe84e256a65043c660f1c682e396
Isaac Araujo <isaac_maciel@hotmail.com>
2020-07-04 12:07:19 -0300

    Finalizaçao da API do Projeto

---
+++ backend/src/controllers/SearchCOntroller.js
@@ -0,0 +1,28 @@
+const { index } = require("../models/utils/PointSchema");
+const parseStringAsArray = require("../utils/parseStringArray");
+const Dev = require("../models/Dev");
+
+module.exports = {
+  async index(req, res) {
+    const { latitude, longitude, techs } = req.query;
+
+    const techsArray = parseStringAsArray(techs);
+
+    const devs = await Dev.find({
+      techs: {
+        $in: techsArray,
+      },
+      location: {
+        $near: {
+          $geometry: {
+            type: "Point",
+            coordinates: [longitude, latitude],
+          },
+          $maxDistance: 10000,
+        },
+      },
+    });
+
+    return res.json(devs);
+  },
+};
---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const port = 3333;
+
+const app = express();
+mongoose.connect(
+  "mongodb+srv://devradar:devradar@cluster0.a9au2.mongodb.net/devradar?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(port, () => {
+  console.log(`Servidor Back rodando na porta ${port}`);
+});
